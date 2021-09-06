6222d3fd450e92f621a5d08ee89e57ffb01e7165
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2021-09-07 00:29:29 +0900

    git init

---
+++ AkkaStudy02/MyUntypedActor.cs
@@ -0,0 +1,12 @@
+﻿using Akka.Actor;
+
+namespace AkkaStudy02
+{
+    public class MyUntypedActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            
+        }
+    }
+}
---
+++ AkkaStudy02/Program.cs
@@ -0,0 +1,24 @@
+﻿using Akka.Actor;
+using System;
+
+namespace AkkaStudy02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActorSystem system = ActorSystem.Create("my-first-akka");
+
+            Props typedActorProps = Props.Create<MyTypedActor>();
+            Props untypedActorProps = Props.Create<MyUntypedActor>();
+
+            IActorRef typedActor = system.ActorOf(typedActorProps);
+            IActorRef untypedActor = system.ActorOf(untypedActorProps);
+
+            /*
+            IActorRef typedActor = system.ActorOf<MyTypedActor>();
+            IActorRef untypedActor = system.ActorOf<MyUntypedActor>();
+            */
+        }
+    }
+}
