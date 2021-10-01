999cabce5a19ae1a044d0da97aefb6af666e4a23
bbangwon <bbangwon.chung@gmail.com>
2021-10-01 18:44:25 +0900

    akka route

---
+++ AkkaRouters/CalculatorActor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace AkkaRouters
+{
+    public class CalculatorActor : ReceiveActor
+    {
+        public CalculatorActor()
+        {
+            Receive<Add>(HandleAddition);
+        }
+
+        public void HandleAddition(Add add)
+        {
+            Console.WriteLine($"{Self.Path} received the request: {add.Term1}+{add.Term2}");
+
+            if(!(Sender is DeadLetterActorRef))
+                Sender.Tell(new Answer(add.Term1 + add.Term2));
+        }
+        
+    }
+
+    public class Answer
+    {
+        public Answer(double value)
+        {
+            this.Value = value;
+        }
+
+        public double Value;
+    }
+
+    public class Add
+    {
+        public Add(double term1, double term2)
+        {
+            this.Term1 = term1;
+            this.Term2 = term2;
+        }
+
+        public double Term1;
+        public double Term2;
+    }
+}
---
+++ AkkaRouters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Routing;
+
+namespace AkkaRouters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActorSystem system = ActorSystem.Create("my-first-akka");
+            var calculatorProps = Props.Create<CalculatorActor>()
+                                    .WithRouter(new ConsistentHashingPool(4)
+                                    .WithHashMapping(x =>
+                                    {
+                                        if(x is Add)
+                                        {
+                                            return ((Add)x).Term1;
+                                        }
+                                        return x;
+                                    }));
+                                    
+
+            var calculatorRef = system.ActorOf(calculatorProps, "calculator");
+
+            calculatorRef.Tell(new Add(100, 20));
+            calculatorRef.Tell(new Add(100, 30));
+            calculatorRef.Tell(new Add(12, 40));
+            calculatorRef.Tell(new Add(100, 10));
+            calculatorRef.Tell(new Add(14, 25));
+
+            Console.Read();
+            system.Terminate();
+        }
+    }
+}
